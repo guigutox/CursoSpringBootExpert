9396e6da6181f75ae34b0989c249147bbde6763d
Guilherme <guilhermegutofer@gmail.com>
2025-02-11 15:02:06 -0300

    Se aplicado, esse commit irÃa adicionar o put e o delete de autor ja funcionais

--- libraryapi/src/main/java/io/github/guigutox/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/io/github/guigutox/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import io.github.guigutox.libraryapi.model.Autor;
 import io.github.guigutox.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,8 +22,32 @@ public class AutorService {
         return repository.save(autor);
     }
 
+    public void atualizar(Autor autor){
+        if(autor.getId() == null){
+            throw new IllegalArgumentException("O id do autor não pode ser nulo");
+
+        }
+        repository.save(autor);
+    }
+
     public Optional<Autor> obterPorId(UUID id){
         return repository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        } else if(nome != null){
+            return repository.findByNome(nome);
+        } else if(nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        } else {
+            return repository.findAll();
+        }
     }
 
 }
